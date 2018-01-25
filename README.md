8e0d2b67f7dc1c6f7326c661f6fa2adc9397a9cc
catlover91 <7215827+CatLover91@users.noreply.github.com>
2018-01-25 17:19:27 +0000

    time to give waifu.church some love

--- README.md
+++ README.md
@@ -4,12 +4,19 @@ The ultimate place of worship
 ## What is it?
 This is a placeholder website designed to give you waifus _on demand_. Mainly this is for people who enjoy that rice aesthetic of transparent png waifus on flat backgrounds, [like on catbox.moe](https://catbox.moe). Current usage for this software isn't live since it was initially something for me to learn lapis on, but rolling your own version of the software gives the two following routes:
 
-`https://url.edu/300/x`: gives an image with a width of 300px and the height scaled to keep the original aspect ratio (can't be croppin our waifus)
+`https://waifu.church/300/x`: gives an image with a width of 300px and the height scaled to keep the original aspect ratio (can't be croppin our waifus)
 
-`https://url.edu/x/300`: gives an image with a height of 300 and the width scaled instead
+`https://waifu.church/x/300`: gives an image with a height of 300 and the width scaled instead
+
+## Running from source
+You need Nodejs, Yarn, and imagemagick installed
+
+Run `yarn` to install dependencies, then run the program (haven't decided on this process yet)
 
 ## Todo
-
+* style errthang
+* Add waifus
 * Add more routes
-* add caching in a sensical way
-* add waifu-specific routes (like `url.edu/miia/300/x`) without caching demolishing my wallet
+* Optimize serving
+* Optimize parsing
+* Waifu Browser™
---
+++ index.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import gm from 'gm'
+import fs from 'fs'
+
+const srcDir = '/waifus'
+// const outDir = '/out'
+
+const maxAge = 3153600
+const exp = new Date(0)
+const defaultExt = 'png'
+
+gm.subClass({ imageMagick: true })
+
+const app = express()
+
+function getUrl (req) {
+  let url = req.headers['X-Forwarded-Protocol'] ? 'https://' : 'http://'
+
+  url += req.headers['host'] + req.url
+
+  return url
+}
+
+app.configure(() => {
+  app.use(express.static(__dirname + '/assets'))
+  // app.use(express.favicon())
+  app.use(app.router)
+
+  // 404 page
+  app.use((req, res) => {
+    res.status('404').render('404', {url: getUrl(req)})
+  })
+
+  // 500 page
+  app.use((err, req, res) => {
+    res.status(err.status || 500).render('500', {error: err})
+  })
+})
+
+// root index
+app.get('/', (req, res) => {
+  res.render('index', {
+    url: getUrl(req)
+  })
+})
+
+app.get('/:width/:height/:format?', (req, res) => {
+  // configure request
+  const ext = typeof req.params.format !== 'undefined' ? req.params.format : defaultExt
+
+  res.setHeader('Content-Type', 'image/' + ext)
+  res.setHeader('Cache-Control', 'public, max-age=' + maxAge)
+  res.setHeader('Expires', exp)
+  res.setHeader('Last-Modified', exp)
+
+  // parse image
+
+  const imgPath = 'test.png'
+
+  const stream = fs.createReadStream(srcDir + imgPath)
+
+  gm(stream, imgPath).stream().on('data', (chunk) => {
+    res.write(chunk)
+  }).on('end', (chunk) => {
+    res.write(chunk)
+  })
+})
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080')
+})
